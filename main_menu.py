7d9a74a4f510901288929637592aca27984b5476
longarmin <github4392@arminlanger.com>
2022-10-24 22:36:37 +0200

    some refactoring and Day2 solved

--- main_menu.py
+++ main_menu.py
@@ -1,13 +1,26 @@
 import sys
 import os
 # sys.path.append(os.path.abspath("./one"))
-scriptpath = "./1"
-sys.path.append(os.path.abspath(scriptpath))
-import a as quest1a
-import b_menu as quest1b
-inp_menu = input("please type in quest number. Synatx:\n(Quest no.(1-25))+(Part no.(a or b))\n")
+import Day1.a
+import Day1.b_menu
+import Day2.a
+import Day2.b
+
+# scriptpath = "./1"
+# sys.path.append(os.path.abspath(scriptpath))
+# import a as quest1a
+# import b_menu as quest1b
+# scriptpath = "./2"
+# sys.path.append(os.path.abspath(scriptpath))
+# import a as quest2a
+inp_menu = input("please type in quest number. Synatx:\n(Quest no.(1-25))+(Part no.(a or b))\n\
+    examples: 1a, 1b, 12a, 24b, ...")
 print("you chose option " + str(inp_menu))
 if(inp_menu == '1a'):
-    quest1a.a()
+    Day1.a.a()
 if(inp_menu == '1b'):
-    quest1b.b()
+    Day1.b_menu.b()
+if(inp_menu == '2a'):
+    Day2.a.a()
+if(inp_menu == '2b'):
+    Day2.b.b()
