d7321d888d44b1f9213eab4ae3aec1701fea78f1
longarmin <github4392@arminlanger.com>
2022-10-25 19:11:43 +0200

    Day 3a completed, but not beatiful

--- main_menu.py
+++ main_menu.py
@@ -5,6 +5,7 @@ import Day1.a
 import Day1.b_menu
 import Day2.a
 import Day2.b
+import Day3.a
 
 # scriptpath = "./1"
 # sys.path.append(os.path.abspath(scriptpath))
@@ -24,3 +25,5 @@ if(inp_menu == '2a'):
     Day2.a.a()
 if(inp_menu == '2b'):
     Day2.b.b()
+if(inp_menu == '3a'):
+    Day3.a.a()
