465ae4653d4cb181d082e98e412151ecd6fb0619
longarmin <github4392@arminlanger.com>
2022-11-03 19:53:15 +0100

    script now creates dirs and files for new days

--- get_inp.sh
+++ get_inp.sh
@@ -4,5 +4,28 @@
 echo 'Day'$1
 DIR='Day'$1
 echo $DIR
+pwd_last_folder=${PWD##*/}
+if [ $pwd_last_folder != "2021_python"]; then
+    echo "please go to dir ''2021_python'' first."
+    exit 1
+fi
+if [ ! -d "$DIR" ]; then 
+    mkdir $DIR
+fi
 cd $DIR
 curl https://adventofcode.com/2021/day/$1/input --cookie "session=$2" > input.txt
+if [ ! -f "a.py" ]; then
+    touch a.py
+else
+    echo "a.py already exists, didn't touch anything!"
+fi
+if [ ! -f "b.py" ]; then
+    touch b.py
+else
+    echo "b.py already exists, didn't touch anything!"
+fi
+if [ ! -f "testinput.txt" ]; then
+    touch testinput.txt
+else
+    echo "testinput.txt already exists, didn't touch anything!"
+fi
