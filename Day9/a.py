2f0fc8b89d6169cb500fbe323f1785f9f30f5f95
longarmin <github4392@arminlanger.com>
2022-11-06 01:19:43 +0100

    Day9a optimized

--- Day9/a.py
+++ Day9/a.py
@@ -1,17 +1,14 @@
-def cmp(a, b):
-    return (a > b) - (a < b)
-
 def main(inp):
     import copy
     risklevel = 0
     mat = [list(map(int,(list(line.strip())))) for line in inp]
-    tens = [10 for i in mat[1]]
+    nines = [9 for i in mat[1]]
     mat_exp = copy.deepcopy(mat)
-    mat_exp.append(copy.deepcopy(tens))
-    mat_exp.insert(0, copy.deepcopy(tens))
+    mat_exp.append(copy.deepcopy(nines))
+    mat_exp.insert(0, copy.deepcopy(nines))
     for i in mat_exp:
-        i.insert(0,10)
-        i.append(10)
+        i.insert(0,9)
+        i.append(9)
     for i in range(1,len(mat_exp)-1):
         for j in range(1,len(mat_exp[i])-1):
             left  = mat_exp[i][j] - mat_exp[i][j-1]
